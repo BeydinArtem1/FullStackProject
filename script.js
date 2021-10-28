ebc35675e2d6d5aeaee2ef1a32907f1b643c3efb
Artem Beydin <artbeidin.exceedteam@gmail.com>
2021-10-28 12:22:05 +0300

    create .css file. add some styles

--- script.js
+++ script.js
@@ -32,42 +32,39 @@ const onClickButton = () => {
   input.value = '';
   summ.value = '';
   render();
-  };
+};
 
 const render = () => {
   const content = document.getElementById('content');
-	while (content.firstChild) {
-		content.removeChild(content.firstChild);
-	};
+  while (content.firstChild) {
+    content.removeChild(content.firstChild);
+  };
   allValues.map((item, index) => {
     const container = document.createElement('div');
-		container.id = `value-${index}`;
-		container.className = 'values-container';
-    const num = document.createElement('p'); 
+    container.id = `value-${index}`;
+    container.className = 'values-container';
+    const num = document.createElement('p');
     num.innerText = `${index + 1})`
     container.appendChild(num);
     const text = document.createElement('p');
-		text.innerText = item.text;
+    text.innerText = item.text;
     container.appendChild(text);
     const date = document.createElement('p');
-		date.innerText = item.date;
+    date.innerText = item.date;
     container.appendChild(date);
     const summ = document.createElement('p');
-		summ.innerText = `${item.summ} p.`;
+    summ.innerText = `${item.summ} p.`;
     container.appendChild(summ);
     const imageEdit = document.createElement('i');
-		imageEdit.className = 'far fa-edit';
-		container.appendChild(imageEdit);
+    imageEdit.className = 'far fa-edit';
+    container.appendChild(imageEdit);
     const imageDelete = document.createElement('i');
-		imageDelete.className = 'far fa-trash-alt';
-		container.appendChild(imageDelete); 
-    content.appendChild(container); 
+    imageDelete.className = 'far fa-trash-alt';
+    container.appendChild(imageDelete);
+    content.appendChild(container);
     count = count + Number(item.summ);
-    
   })
-   
-  const countRender = document.getElementById('countOf'); 
-  console.log(countRender)
+  const countRender = document.getElementById('countOf');
   countRender.innerText = count;
   count = 0;
 }
